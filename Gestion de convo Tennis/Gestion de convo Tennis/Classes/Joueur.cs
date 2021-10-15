0c3cefeba7676c19b7226397ad7ca7d850c1ae19
Thaskow <lucas.surma@hotmail.com>
2021-10-15 10:14:33 +0200

    Modif des Int en int

--- Gestion de convo Tennis/Gestion de convo Tennis/Classes/Joueur.cs
+++ Gestion de convo Tennis/Gestion de convo Tennis/Classes/Joueur.cs
@@ -10,14 +10,14 @@ namespace Gestion_de_convo_Tennis.Classes
     {
         public String Nom { get; set; }
         public String Prenom { get; set; }
-        public Int Age { get; set; }
+        public int Age { get; set; }
         public String License { get; set; }
         public String Certificat { get; set; }
         public Boolean Categorie { get; set; }
 
         public Joueur(){}
 
-        public Joueur(String nom, String prenom, Int age, String license, String certificat, Boolean categorie)
+        public Joueur(String nom, String prenom, int age, String license, String certificat, Boolean categorie)
         {
             this.Nom = nom;
             this.Prenom = prenom;
