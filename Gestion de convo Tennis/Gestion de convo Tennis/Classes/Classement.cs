df3438d1f32d2655be58bf2b60b69df93977431c
Thaskow <smick9@hotmail.fr>
2021-10-15 10:21:16 +0200

    test

--- Gestion de convo Tennis/Gestion de convo Tennis/Classes/Classement.cs
+++ Gestion de convo Tennis/Gestion de convo Tennis/Classes/Classement.cs
@@ -12,8 +12,8 @@ namespace Gestion_de_convo_Tennis.Classes
         public int Rang {get;set;}
         public Classement (){}
         public Classement (int index, int rang) {
-            this.Index=index;
-            this.Rang=rang;
+            this.Index = index;
+            this.Rang = rang;
         }
         public override string ToString() {
             return "Index : " + this.Index + "\nRang : " + this.Rang;
