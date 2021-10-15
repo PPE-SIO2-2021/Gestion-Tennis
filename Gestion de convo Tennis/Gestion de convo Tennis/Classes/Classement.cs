82acbc12f4470700e05c0ee1293df6329ff643c6
Robin Liard <robinliard@outlook.fr>
2021-10-15 10:38:21 +0200

    Ajout de l'id dans la classe Classement

--- Gestion de convo Tennis/Gestion de convo Tennis/Classes/Classement.cs
+++ Gestion de convo Tennis/Gestion de convo Tennis/Classes/Classement.cs
@@ -8,15 +8,18 @@ namespace Gestion_de_convo_Tennis.Classes
 {
     public class Classement
     {
+        public int Id { get; set; }
         public int Index {get;set;}
         public int Rang {get;set;}
+        
         public Classement (){}
-        public Classement (int index, int rang) {
+        public Classement (int unId,int index, int rang) {
+            this.Id = unId;
             this.Index = index;
             this.Rang = rang;
         }
         public override string ToString() {
-            return "Index : " + this.Index + "\nRang : " + this.Rang;
+            return "ID : " + Id + " Index : " + this.Index + "\nRang : " + this.Rang;
         }
     }
 }
