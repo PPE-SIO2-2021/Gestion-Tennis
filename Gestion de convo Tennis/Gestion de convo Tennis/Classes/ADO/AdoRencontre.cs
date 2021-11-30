63c82b0f900ebe1a978244bcfb5730f6f8318790
RGDayan <90703955+RGDayan@users.noreply.github.com>
2021-11-30 10:55:50 +0100

    RE-Push des modifs d'adoJournee pour les dispos

--- Gestion de convo Tennis/Gestion de convo Tennis/Classes/ADO/AdoRencontre.cs
+++ Gestion de convo Tennis/Gestion de convo Tennis/Classes/ADO/AdoRencontre.cs
@@ -24,6 +24,45 @@ namespace Gestion_de_convo_Tennis.Classes
             reader.Close();
             return rencontres;
         }
+
+        public static void addRencontre(List<Journee> journees)
+        {
+            SqlCommand cmd = new SqlCommand("INSERT INTO rencontre(adversaire,lieu,dte_rencontre,fk_id_journee,fk_id_equipe) VALUES(@adversaire,@lieu,@dte_rencontre,@heure,@fk_id_journee,@fk_id_equipe)");
+            cmd.Connection = Ado.OpenSqlConnection();
+            cmd.Prepare();
+            foreach (Journee journee in journees)
+            {
+                foreach (Rencontre rencontre in journee.Rencontres)
+                {
+                    cmd.Parameters.AddWithValue("@adversaire", rencontre.Adversaire);
+                    cmd.Parameters.AddWithValue("@lieu", rencontre.Lieu);
+                    cmd.Parameters.AddWithValue("@dte_rencontre", rencontre.DteHeure); // Chercher juste la date // Chercher que l'heure
+                    cmd.Parameters.AddWithValue("@fk_id_journee", journee.Id);
+                    cmd.Parameters.AddWithValue("@fk_id_equipe", rencontre.Equipe.Id);
+                    cmd.ExecuteNonQuery();
+                    SqlCommand cmdJournee = new SqlCommand("INSERT INTO journee(dte, categorie) VALUES(@dte, @categorie); SELECT SCOPE_IDENTITY();");
+                    cmdJournee.Connection = Ado.OpenSqlConnection();
+                    cmdJournee.Prepare();
+                    cmdJournee.Parameters.AddWithValue("@dte", journee.Date);
+                    cmdJournee.Parameters.AddWithValue("@categorie", journee.Categorie.Trim());
+                    journee.Id = Convert.ToInt32(cmdJournee.ExecuteScalar());
+
+                    foreach (var j in journee.Dispo)
+                    {
+                        SqlCommand cmdDispo = new SqlCommand("INSERT INTO disponible(fk_id_joueur, fk_id_journee, is_dispo) VALUES (@id_joueur, @id_journee, @dispo); ");
+                        cmdDispo.Connection = Ado.OpenSqlConnection();
+                        cmdDispo.Prepare();
+                        cmdDispo.Parameters.AddWithValue("@id_joueur", j.Key.Id);
+                        cmdDispo.Parameters.AddWithValue("@id_journee", journee.Id);
+                        cmdDispo.Parameters.AddWithValue("@dispo", j.Value);
+                        cmdDispo.ExecuteNonQuery();
+                        cmdDispo.Connection.Close();
+                    }
+                    cmdJournee.Connection.Close();
+                }
+            }
+        }
+
         public static void delete()
         {
             SqlCommand cmd = new SqlCommand("DELETE FROM rencontre; DBCC CHECKIDENT (rencontre, RESEED, 0);");
